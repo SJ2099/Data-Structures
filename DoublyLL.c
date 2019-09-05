2f35a2012023eff3ad87f237dad16b221fcde682
Shweta <46452833+SJ592@users.noreply.github.com>
2019-09-05 20:12:19 +0530

    updated

--- DoublyLL.c
+++ DoublyLL.c
@@ -89,26 +89,6 @@ void DeleteFromTail()
 	}
 	printf("Node Deleted\n");
 }
-void InsertBefore(int x)         // error
-{
-	int loc;
-	struct node *newnode=GetNewNode(x);
-	struct node *temp=head;
-	printf("Enter node before which node is to be inserted:");
-	scanf("%d", &loc);
-	if(head==NULL)
-		head=newnode;
-	while(temp->data!=loc)
-	{
-		temp=temp->next;
-	}
-	temp->prev=newnode;
-	temp->prev->next=newnode;   //temp's initial prev before insertion of newnode
-	newnode->prev=temp->prev;
-	newnode->next=temp;
-	printf("Node Inserted\n");
-
-}
 void InsertAfter(int x)
 {
 	int loc;
@@ -174,7 +154,7 @@ int main()
 	clrscr();
 	do
 	{
-	printf("\n1.Insert at Head\n2.Insert at Tail\n3.Delete from Head\n4.Delete from Tail\n5.Insert Before\n6.Insert After\n7.Remove Specific\n8.Display\n9.Display Reverse\n");
+	printf("\n1.Insert at Head\n2.Insert at Tail\n3.Delete from Head\n4.Delete from Tail\n5.Insert After\n6.Remove Specific\n7.Display\n8.Display Reverse\n");
 	printf("Enter your choice:");
 	scanf("%d", &choice);
 	switch(choice)
@@ -198,28 +178,23 @@ int main()
 		case 5:
 		printf("Enter element:");
 		scanf("%d", &x);
-		InsertBefore(x);
+		InsertAfter(x);
 		break;
 		case 6:
 		printf("Enter element:");
 		scanf("%d", &x);
-		InsertAfter(x);
-		break;
-		case 7:
-		printf("Enter element:");
-		scanf("%d", &x);
 		RemoveSpecific(x);
 		break;
-		case 8:
+		case 7:
 		Display();
 		break;
-		case 9:
+		case 8:
 		DisplayRev();
 		break;
 		default:
 		printf("Invalid Choice\n");
 		break;
 	}
-	}while(choice!=10);
+	}while(choice!=9);
 	return 0;
 }
